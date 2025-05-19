eb678480345e29cb2e76a16a321ac9df274f05d6
jsbaasi <jujhaar02@gmail.com>
2025-05-19 15:06:33 +0100

    iteratively but not collecting the case where only the happy paths gets collected

--- day6.py
+++ day6.py
@@ -54,12 +54,6 @@ class Solution:
 
     def part2(self, grid):
         ROWS, COLS = len(grid), len(grid[0])
-        dirs = {
-            "v": (1, 0),
-            "^": (-1, 0),
-            ">": (0, 1),
-            "<": (0, -1),
-        }
         alter: dict[tuple] = {
             (-1, 0): (1, 1),
             (0, 1): (1, -1),
@@ -73,66 +67,65 @@ class Solution:
             (0, 1): (1, 0),
         }
 
-        upSet = set()
-        downSet = set()
-        rightSet = set()
-        leftSet = set()
-
-        dictOfSets = {
-            (1, 0): downSet,
-            (-1, 0): upSet,
-            (0, 1): rightSet,
-            (0, -1): leftSet,
+        dirToArrow = {
+            (0, 1): ">",
+            (0, -1): "<",
+            (1, 0): "v",
+            (-1, 0): "^",
         }
+
         res = 0
 
-        def dfs(r, c, dir: tuple):
+        def checkCell(r, c):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS:
-                return
-            if grid[r][c] == ".":
-                s = grid[r][:c] + "," + grid[r][c + 1 :]
-                grid[r] = s
-                dictOfSets[dir].add((r, c))
-            turnDir = turn[dir]
-            if (0 <= r + turnDir[0] < ROWS) and (0 <= c + turnDir[1] < COLS):
-                toMyRight = (r + turnDir[0], c + turnDir[1])
-                toMyRightGrid = grid[r + turnDir[0]][c + turnDir[1]]
-                if toMyRightGrid == ",":
-                    if toMyRight in dictOfSets[turnDir]:
-                        nonlocal res
-                        res += 1
-            if (0 <= r + dir[0] < ROWS) and (0 <= c + dir[1] < COLS):
-                if grid[r + dir[0]][c + dir[1]] == "#":
-                    dfs(
-                        r + turnDir[0],
-                        c + turnDir[1],
-                        turnDir,
-                    )
-            dfs(
-                r + dir[0],
-                c + dir[1],
-                dir,
-            )
+                return False
+            return True
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] in dirs:
-                    dir = dirs[grid[r][c]]
-                    s = grid[r][:c] + "," + grid[r][c + 1 :]
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "^":
+                    # s = grid[row][:col] + "." + grid[row][col + 1 :]
+                    # grid[row] = s
+                    r, c = row, col
+        dir = (-1, 0)
+
+        f = open("Day6Debug.txt", "a")
+        while checkCell(r, c):
+            if grid[r][c] == "#":
+                alteration = alter[dir]
+                dir = turn[dir]
+                r, c = r + alteration[0], c + alteration[1]
+            else:
+                turnRightDir = turn[dir]
+                turnRightTuple = (r + turnRightDir[0], c + turnRightDir[1])
+                if (
+                    grid[turnRightTuple[0]][turnRightTuple[1]]
+                    == dirToArrow[turnRightDir]
+                ):
+                    res += 1
+
+                if grid[r][c] == ".":
+                    s = grid[r][:c] + dirToArrow[dir] + grid[r][c + 1 :]
                     grid[r] = s
-                    upSet.add((r, c))
-                    dfs(r, c, dir)
-                    return res
+                else:
+                    s = grid[r][:c] + dirToArrow[dir] + grid[r][c + 1 :]
+                    grid[r] = s
+
+                r, c = r + dir[0], c + dir[1]
+
+            gridcopy = "\n".join(grid)
+            f.write(gridcopy + "\n" + str(res) + "\n\n")
+        return res
 
 
 grid = []
-with open("day6RawInput.txt") as f:
+with open("day6TestInput.txt") as f:
     for line in f:
         grid.append(line.rstrip())
 
 obj = Solution()
+# print(obj.part1(grid.copy()))  # 0.01 seconds
 t1 = time.time()
-print(obj.part1(grid.copy()))  # 0.01 seconds
+print(obj.part2(grid.copy()))
 t2 = time.time()
 print(t2 - t1)
-# print(obj.part2(grid))
